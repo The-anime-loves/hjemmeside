4a5125833e3861544f2f311d07916fc46367801d
noahkuhne12 <50324963+noahkuhne12@users.noreply.github.com>
2019-10-09 08:41:56 +0200

    add som chenstes

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 <html>
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="/A_Z_Webshop/css/index.css">
-        <link rel="stylesheet" type="text/css" href="/A_Z_Webshop/css/navbar.css">
+        <link rel="stylesheet" type="text/css" href="/css/index.css">
+        <link rel="stylesheet" type="text/css" href="/css/navbar.css">
         <meta charset="UTF-8">
     </head>
     <body>
@@ -15,7 +15,7 @@
             echo Navbar::buildNav();
         ?>
         <div class="container">
-            <img src="/A_Z_Webshop/img/index/webshop_welcomeside.png" >
+            <img src="/img/index/webshop_welcomeside.png" >
         </div>
         <?php
             require 'html/foot.html';
