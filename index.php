b319ddfc0af33d15799fe3e853fc37b3af44da78
Will1331 <Will1331@SKP-PC002752.skpit.local>
2019-10-09 10:35:09 +0200

    something u already did

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 <html>
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="/css/index.css">
-        <link rel="stylesheet" type="text/css" href="/css/navbar.css">
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+        <link rel="stylesheet" type="text/css" href="css/navbar.css">
         <meta charset="UTF-8">
     </head>
     <body>
