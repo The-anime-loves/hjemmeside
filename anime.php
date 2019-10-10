cf148086f108ecc27202b61033fc20ce2a6f7c4a
noahkuhne12 <50324963+noahkuhne12@users.noreply.github.com>
2019-10-10 08:37:54 +0200

    add som chenstes

---
+++ anime.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();    
+    require 'class/navbar.php';
+    require 'class/productTabel.php';
+    require 'class/mySQL.php';
+    
+    
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/navbar.css">
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+        echo Navbar::buildNav();
+        ?>
+        <?php
+        echo productTabel::buildProductTabel();
+        ?>
+        <?php
+            require 'html/foot.html';
+        ?>
+    </body>
+</html>
